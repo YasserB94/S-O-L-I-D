c378066cdad9addd274dc0038c90594f224f7905
Yasser Bal <yasserbal@icloud.com>
2022-08-01 12:00:21 +0200

    Adding main.ts Contains refactored code from old.ts Hardcoded strings defined in associative Array to extend scale- and maintainability

---
+++ my-solutions/0_S/main.ts
@@ -0,0 +1,80 @@
+import { Car } from "./Models/Car"
+
+// When you see <cast>variable this is a "cast" of a variable, explicitly telling the code what the type of this variable will be.
+// This is sometimes needed when a default JS function does not return a precise enough Type.
+// I need to cast this to HtmlElement because the default Element return type is not specific to the HTML context (because some versions of JS can also be used in the backend, see node.js)
+// This makes it not having some properties like .innerText. Test it out yourself by removing the <HTMLElement>
+const musicToggleElement = <HTMLElement>document.querySelector('#music-toggle');
+const musicSliderElement = <HTMLInputElement>document.querySelector('#music-slider');
+const engineToggleElement = <HTMLInputElement>document.querySelector('#engine-toggle');
+const addFuelForm = <HTMLFormElement>document.querySelector('#add-fuel-form');
+const addFuelInput = <HTMLFormElement>document.querySelector('#add-fuel-input');
+const fuelLevelElement = <HTMLElement>document.querySelector('#fuel-level');
+const milesElement = <HTMLElement>document.querySelector('#miles-value');
+const audioElement = <HTMLAudioElement>document.querySelector('#car-music');
+
+let car = new Car(100);
+const arrCarStatusMessages = {
+    'radio':{
+        turnOn:'Turn music on',
+        turnOff:'Turn music off'
+    },
+    'engine':{
+        turnOn:'Turn engine on',
+        turnOff:'Turn engine off'
+    }
+}
+musicToggleElement.addEventListener('click', () => {
+    if(car.musicPlayer.volume === 0) {
+        car.musicPlayer.turnMusicOn();
+        musicSliderElement.value = car.musicPlayer.volume.toString();
+        musicToggleElement.innerText = arrCarStatusMessages['radio'].turnOff;
+        return;
+    }
+    musicToggleElement.innerText = arrCarStatusMessages['radio'].turnOn;
+    car.musicPlayer.turnMusicOff();
+});
+
+//I use input instead of change, because then the value changes when I move the mouse, not only on release
+musicSliderElement.addEventListener('input', (event) => {
+    let target = <HTMLFormElement>(event.target);
+
+    car.musicPlayer.volume = target.value;
+    audioElement.volume = car.musicPlayer.volume / 100;
+
+    musicToggleElement.innerText = car.musicPlayer.volume ? arrCarStatusMessages['radio'].turnOn : arrCarStatusMessages['radio'].turnOff;
+});
+
+engineToggleElement.addEventListener('click', () => {
+    if(car.engine.status) {
+        car.engine.turnOff();
+        engineToggleElement.innerText = arrCarStatusMessages['engine'].turnOn;
+        return;
+    }
+    engineToggleElement.innerText = arrCarStatusMessages['engine'].turnOff;
+    car.engine.turnOn();
+});
+
+addFuelForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    car.fueltank.addFuel(Number(addFuelInput.value));
+    fuelLevelElement.innerText = car.fueltank.amountOfFuel.toString();
+});
+
+setInterval(() => {
+    car.drive();
+
+    //while it looks like both lines below are the same there is a subtle difference (you could put breakpoints here to see the difference):
+    // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
+    milesElement.innerText = <string><unknown>(car.odoMeter.count);
+    // This .toString() will actually convert the value in JavaScript from an integer to a string
+    fuelLevelElement.innerText = car.fueltank.amountOfFuel.toString();
+
+    if(car.musicPlayer.volume === 0) {
+        audioElement.pause();
+    } else {
+        audioElement.play();
+    }
+
+}, 1000);
